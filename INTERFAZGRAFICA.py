15aa65fc849f9e533993d46f754d8ba654bfd3a6
steven <sibajasteven7@gmail.com>
2021-04-18 23:38:22 -0600

    cambios
    estaba mal el archivo

--- INTERFAZGRAFICA.py
+++ INTERFAZGRAFICA.py
@@ -1,14 +1,5 @@
+
 import tkinter
-
-
-
-#codigo de fibonacci
-
-def fibonac():
-     return fibonac()
-    
-
-    
 
 def fibonacci(num):
     if (isinstance (num,int) and (num>=0)):
@@ -22,55 +13,90 @@ def fibonacci_aux(num):
         return 1
     else:
         return fibonacci_aux (num-1) + fibonacci_aux (num-2)
+# funcion para para cambiar a la ventana fibonacci
+def ir_fibo():
+    inicial.forget()
+    ventana_fibo.pack(fill='both',expand=1)
 
-    
-#funcion para informacion.
-def info():
-    return info
-# funcion para animacion.
-ani=6
-def ani():
-    return ani()
-# crear funcion para atras
+
+#funcion para boton de atras
 def atras():
-    return atras()
+    ventana_fibo.forget()
+    ventana_info.forget()
+    inicial.pack(fill='both',expand=1)
+
+def ir_info():
+    inicial.forget()
+    ventana_info.pack(fill='both',expand=1)
 
 
 
-#creamos las ventanas
 
+
+
+ ###ventanas####
     
-def main():
-    # Window settings
-    window = tkinter.Tk()
-    window.title("interfas grafica")
-    window.geometry("800x450")
- #   window.iconbitmap("icon.ico")
-    window.resizable(False, False)
-    window.config(cursor='heart')
+ventana =  tkinter.Tk() #ventana padre
+ventana.geometry("600x400")
+ventana.config(cursor='heart')
+ventana.title("interfaz grafica-steven sibaja")
+
+###ventanas hijas###
+inicial= tkinter.Frame(ventana)# ventana principal
+ventana_fibo = tkinter.Frame(ventana) #ventana fibo
+ventana_info = tkinter.Frame(ventana)
 
 
-    
-# Canvas settings
-    pannel_canvas = tkinter.Canvas(window, width=274, height=446, borderwidth=0, highlightthickness=0, bg="orange")
-    pannel_canvas.place(x=0, y=2)
 
-    anim_canvas = tkinter.Canvas(window, width=524, height=446, borderwidth=0, highlightthickness=0, bg="red")
-    anim_canvas.place(x=276, y=2)
+ ####Label####
+label_fibo=tkinter.Label(ventana_fibo,text='ventana nueva')# se crea una ventana nueva
+label_fibo.pack(pady=20)# corre el texto con base al eje y
+label_inicial = tkinter.Label(inicial,text= "botones de comando")# primer ventana en aparecer
+label_inicial.pack(pady=20) # se utiliza para el espacio del texto hacai abajo
+label_info = tkinter.Label(ventana_info,text= "info")# primer ventana en aparecer de info
+label_info.pack(pady=20) # se utiliza pa
 
-   
+    #####botones####
 
-     #Canvas components
-    label = tkinter.Label(pannel_canvas, text="Press to start the animation", font=("Haettenschweiler", 15), bg="#606060", fg=White)
-    label.place(x=25, y=25)
-    boton_fibonacci = tkinter.Button(pannel_canvas, text="fibonacci", command=fibonac,bg=("red"))
-    boton_fibonacci.place(x=25, y=50)
-    boton_informacion= tkinter.Button(pannel_canvas,text= "informacion personal",command=info, bg = "sky blue")
-    boton_informacion.place(x=25, y=150)
-    boton_animacion= tkinter.Button(pannel_canvas,text= "animacion",command= ani, bg = "orange")
-    boton_animacion.place(x=25, y=250)
-    boton_atras= tkinter.Button(pannel_canvas,text="atras",command=atras,bg="white")
-    boton_atras.place(x=25,y=350)
-main()
-main.mainloop()
-      
+boton_fibonacci= tkinter.Button(inicial,text= "fibonacci",command= ir_fibo, bg = "fuchsia") 
+boton_info= tkinter.Button(inicial,text= "informacion personal",command= ir_info, bg = "sky blue")
+boton_ani= tkinter.Button(inicial,text= "animacion", bg = "orange")
+boton_atras_fibo= tkinter.Button(ventana_fibo,text="atras", command= atras, bg="white")
+boton_atras_info= tkinter.Button(ventana_info,text="atras", command= atras, bg="white")
+
+
+boton_fibonacci.pack(pady=10)
+boton_info.pack(pady=20)
+boton_ani.pack(pady=30)
+boton_atras_fibo.pack(pady=40)
+boton_atras_info.pack()
+inicial.pack()
+
+
+                       
+ventana.mainloop()
+
+
+
+
+
+
+
+#width=8,height=4)
+
+
+
+'''
+def fibonacci(num):
+    if (isinstance (num,int) and (num>=0)):
+        return fibonacci_aux(num)
+    else:
+        return ("el numero debe ser engtero y positivo")
+def fibonacci_aux(num):
+    if num==0:
+        return 0
+    elif num==1:
+        return 1
+    else:
+        return fibonacci_aux (num-1) + fibonacci_aux (num-2)
+'''  
